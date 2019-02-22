e3fa0499b1368ebc32087e41e72697699cb86eeb
MohLR <37324952+MohLR@users.noreply.github.com>
2019-02-22 17:02:16 +0700

    Update soal3.sh

--- soal3.sh
+++ soal3.sh
@@ -5,18 +5,4 @@ while [ -f password$i.txt ]
 do
 	let i=$i+1
 done
-pass= head /dev/urandom | tr -cd [:alnum:] | head -c 12
-for password in ls password*.txt
-do
-	echo "$password"
-	value = `cat "$password"`
-	if [ $pass = $value ]
-	then
-		echo "Coba jalankan file lagi, sebelumnya password yang dihasilkan merupakan duplikat sehingga tidak terbuat file baru"
-		break
-	fi
-done
-if [ $pass != $value ]
-then
-	$pass > password$i.txt
-fi
+head /dev/urandom | tr -cd [:alnum:] | head -c 12 > password$i.txt
