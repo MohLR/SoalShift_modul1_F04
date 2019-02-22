399f92e38344030a58fe323e2ad4ffa527477bb6
MohLR <37324952+MohLR@users.noreply.github.com>
2019-02-22 17:10:22 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nama=`date +"%H:%M_%d-%m-%Y"`
+nama=`date +"%H:%M %d-%m-%Y"`
 jam=`date +"%H"`
-cat /var/log/syslog > $nama
+cat /var/log/syslog > "$nama"
 alpha=$( printf "%s" {a..z} )
 part1=$( printf $(printf '\%03o' $(seq $((97+$jam)) 122 ) ) )
 part2=$( printf $(printf '\%03o' $(seq 97 $((97+$jam-1)) ) ) )
-sed -i y/$alpha/$part1$part2/ $nama
+sed -i y/$alpha/$part1$part2/ "$nama"
