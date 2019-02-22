fb1fbb7c8be44075d6132f15967d1f3e3ec3b5a3
MohLR <37324952+MohLR@users.noreply.github.com>
2019-02-22 17:02:41 +0700

    Add files via upload

---
+++ soal4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nama=`date +"%H:%M_%d-%m-%Y"`
+jam=`date +"%H"`
+cat /var/log/syslog > $nama
+alpha=$( printf "%s" {a..z} )
+part1=$( printf $(printf '\%03o' $(seq $((97+$jam)) 122 ) ) )
+part2=$( printf $(printf '\%03o' $(seq 97 $((97+$jam-1)) ) ) )
+sed -i y/$alpha/$part1$part2/ $nama
