fd73aef96e241751de96801a83b2e163d231ec6e
MohLR <37324952+MohLR@users.noreply.github.com>
2019-03-02 22:29:21 +0700

    Add files via upload

--- soal4.sh
+++ soal4.sh
@@ -6,4 +6,8 @@ cat /var/log/syslog > "$nama"
 alpha=$( printf "%s" {a..z} )
 part1=$( printf $(printf '\%03o' $(seq $((97+$jam)) 122 ) ) )
 part2=$( printf $(printf '\%03o' $(seq 97 $((97+$jam-1)) ) ) )
+Alpha=$( printf "%s" {A..Z} )
+Part1=$( printf $(printf '\%03o' $(seq $((65+$jam)) 90 ) ) )
+Part2=$( printf $(printf '\%03o' $(seq 65 $((65+$jam-1)) ) ) )
 sed -i y/$alpha/$part1$part2/ "$nama"
+sed -i y/$Alpha/$Part1$Part2/ "$nama"
--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk /'cron/ || /CRON/,!/sudo/' /var/log/syslog | awk 'NF < 13' > /home/faqih/modul1/syslogikul.log
+awk /'cron/ || /CRON/,!/sudo/ || !/SUDO/' /var/log/syslog | awk 'NF < 13' > ~/Documents/syslogikul.log
